a2395dc6e9b9c238542c2bffdc621572c4c8eadf
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-04-20 11:53:37 -0500

    feat: Configuracion inicial frontend

---
+++ frontend/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+import { deepOrange, lime } from '@material-ui/core/colors'
+
+const theme = createMuiTheme({
+  // palette: {
+  //   primary: {
+  //     main: lime[500],
+  //   },
+  //   secondary: deepOrange,
+  // },
+})
+
+export default theme
