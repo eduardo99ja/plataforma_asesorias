d0f7e6f2100561b0cce4a51e0acbeac6de48f146
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-04-27 19:28:46 -0500

    feat: Show topics

---
+++ frontend/components/card/Card.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+    margin: '10px',
+  },
+  media: {
+    height: 140,
+  },
+})
+
+const CardTopic = () => {
+  const classes = useStyles()
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image='https://res.cloudinary.com/du17vjpcn/image/upload/v1619569005/public_asesorias/materia_prpbyg.png'
+          title='tema'
+        />
+        <CardContent>
+          <Typography gutterBottom variant='h5' component='h2'>
+            Tema
+          </Typography>
+          <Typography variant='body2' color='textSecondary' component='p'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+            laborum at. Assumenda aperiam eum omnis voluptates nihil quos
+            necessitatibus sint!
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size='small' color='primary'>
+          Guardar
+        </Button>
+        <Button size='small' color='primary'>
+          Ver más
+        </Button>
+      </CardActions>
+    </Card>
+  )
+}
+
+export default CardTopic
--- frontend/pages/catalogo.js
+++ frontend/pages/catalogo.js
@@ -1,11 +1,133 @@
 import React from 'react'
 
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import InputLabel from '@material-ui/core/InputLabel'
+import Input from '@material-ui/core/Input'
+import MenuItem from '@material-ui/core/MenuItem'
+import FormControl from '@material-ui/core/FormControl'
+import Select from '@material-ui/core/Select'
+import Layout from '../components/layout/Layout'
+import { Container, Grid } from '@material-ui/core'
+
+import CardTopic from '../components/card/Card'
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+}))
+
 const catalogo = () => {
+  const classes = useStyles()
+  const [open, setOpen] = React.useState(false)
+  const [materia, setMateria] = React.useState('')
+  const [nivel, setNivel] = React.useState('')
+
+  const handleChangeMateria = event => {
+    setMateria(event.target.value || '')
+  }
+  const handleChangeNivel = event => {
+    setNivel(event.target.value || '')
+  }
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+  const SelectDialog = () => {
     return (
-        <div>
-            Hola
-        </div>
+      <div>
+        <Button onClick={handleClickOpen}>Filtrar temas</Button>
+        <Dialog
+          disableBackdropClick
+          disableEscapeKeyDown
+          open={open}
+          onClose={handleClose}
+        >
+          <DialogTitle>Seleccionar filtros</DialogTitle>
+          <DialogContent>
+            <form className={classes.container}>
+              <FormControl className={classes.formControl}>
+                <InputLabel htmlFor='demo-dialog-native'>Nivel</InputLabel>
+                <Select
+                  native
+                  value={nivel}
+                  onChange={handleChangeNivel}
+                  input={<Input id='demo-dialog-native' />}
+                >
+                  <option aria-label='None' value='' />
+                  <option value='sec'>Secundaria</option>
+                  <option value='bach'>Bachillerato</option>
+                  <option value='lic'>Licenciatura</option>
+                </Select>
+              </FormControl>
+              <FormControl className={classes.formControl}>
+                <InputLabel id='demo-dialog-select-label'>Materia</InputLabel>
+                <Select
+                  labelId='demo-dialog-select-label'
+                  id='demo-dialog-select'
+                  value={materia}
+                  onChange={handleChangeMateria}
+                  input={<Input />}
+                >
+                  <MenuItem value=''>
+                    <em>Ninguno</em>
+                  </MenuItem>
+                  <MenuItem value='math'>Matematicas</MenuItem>
+                  <MenuItem value='quim'>Quimica</MenuItem>
+                  <MenuItem value='eng'>Ingles</MenuItem>
+                </Select>
+              </FormControl>
+            </form>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color='primary'>
+              Cancel
+            </Button>
+            <Button onClick={handleClose} color='primary'>
+              Ok
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
     )
+  }
+  return (
+    <Layout>
+      <Container maxWidth='lg'>
+        <Grid container direction='row' justify='center'>
+          <SelectDialog />
+        </Grid>
+        <Grid container>
+          <Grid item container>
+            <h2>Matematicas</h2>
+          </Grid>
+          <Grid item container lg={12}>
+            {[0, 1, 2, 3, 4, 5, 6, 7].map(item => {
+              return (
+                <Grid item lg={3} md={4} sm={6} xm={12}>
+                  <CardTopic />
+                </Grid>
+              )
+            })}
+          </Grid>
+        </Grid>
+      </Container>
+    </Layout>
+  )
 }
 
 export default catalogo
